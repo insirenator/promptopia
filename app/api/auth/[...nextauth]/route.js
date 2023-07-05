052d1427bda546f5c407b687fccb06f91eafc2df
insirenator <arsshakeeb149@protonmail.com>
2023-07-05 17:25:20 +0530

    Added: upto mongodb connection

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+import { connectToDB } from "@utils/database";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    })
+  ],
+
+  async session({ session }) {
+
+  },
+
+  async signIn({ profile}) {
+    try {
+      await connectToDB(); 
+    } catch (error) {
+      console.log(error);
+    }
+  }
+})
+
+export { handler as GET, handler as POST};
