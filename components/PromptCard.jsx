a73d223162336ba5204dbe981db878ccfe4e07be
insirenator <arsshakeeb149@protonmail.com>
2023-07-07 17:42:15 +0530

    Completed Video tut features

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -6,7 +6,17 @@ import { useSession } from 'next-auth/react';
 import { usePathname, useRouter } from 'next/navigation';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete}) => {
+  const { data: session } = useSession();
+  const pathname = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState("");
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(""), 3000);
+  }
 
   return (
     <div className='prompt_card'>
@@ -30,7 +40,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete}) => {
           </div>
         </div>
 
-        <div className='copy_btn' onClick={() => {}}>
+        <div className='copy_btn' onClick={handleCopy}>
           <Image 
             src={copied === post.prompt
               ? "/assets/icons/tick.svg"
@@ -49,8 +59,26 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete}) => {
       <p className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}  
+        {"#" + post.tag}  
       </p>
+
+      {session?.user.id === post.creator._id && 
+      pathname === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-200 pt-3'>
+          <p
+           className='font-inter text-sm green_gradient cursor-pointer'
+           onClick={handleEdit}
+          >
+            Edit
+          </p>
+          <p
+           className='font-inter text-sm orange_gradient cursor-pointer'
+           onClick={handleDelete}
+          >
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
